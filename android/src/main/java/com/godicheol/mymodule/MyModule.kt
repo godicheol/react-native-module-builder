db3a96383b60750ec562c421181efe8328109593
godicheol <godicheol@gmail.com>
2023-07-10 20:32:34 +0900

    update

--- android/src/main/java/com/godicheol/mymodule/MyModule.kt
+++ android/src/main/java/com/godicheol/mymodule/MyModule.kt
@@ -1,18 +1,19 @@
-package com.godicheol.mymodule;
+package com.godicheol.mymodule
 
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.Callback
+import com.facebook.react.bridge.Promise
 
+// edit "MyModule"
 class MyModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
   // To export a module named NativeModules.MyModule.getName
   @ReactMethod(isBlockingSynchronousMethod = true)
-  override fun getName() = "MyModule"
+  override fun getName() = "MyModule" // edit "MyModule"
 
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
--- android/src/main/java/com/godicheol/mymodule/MyPackage.kt
+++ android/src/main/java/com/godicheol/mymodule/MyPackage.kt
@@ -1,4 +1,4 @@
-package com.godicheol.mymodule;
+package com.godicheol.mymodule
 
 import android.view.View
 import com.facebook.react.ReactPackage
@@ -7,6 +7,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
 
+// edit "MyPackage"
 class MyPackage : ReactPackage {
 
   override fun createViewManagers(
@@ -15,6 +16,6 @@ class MyPackage : ReactPackage {
 
   override fun createNativeModules(
     reactContext: ReactApplicationContext
-  ): MutableList<NativeModule> = listOf(MyModule(reactContext)).toMutableList()
+  ): MutableList<NativeModule> = listOf(MyModule(reactContext)).toMutableList() // edit "MyModule"
   
 }
